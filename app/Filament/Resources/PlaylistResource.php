94c399681cf27151e6cc0a82db8003f6e2b3dee8
ThemerV <56089478+ThemerV@users.noreply.github.com>
2024-11-30 02:24:33 -0300

    Add PlaylistResource
    - added playlists to admin panel

---
+++ app/Filament/Resources/PlaylistResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PlaylistResource\Pages;
+use App\Filament\Resources\PlaylistResource\RelationManagers;
+use App\Models\Playlist;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PlaylistResource extends Resource
+{
+    protected static ?string $model = Playlist::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        $record = $form->getRecord();
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user')
+                    ->relationship('user', 'username')
+                    ->disabled()
+                    ->label('Usuário'),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('games')
+                    ->relationship('games', 'name')
+                    ->multiple(),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.username')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Usuário'),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->label('Nome da Playlist'),
+                Tables\Columns\TextColumn::make('games.name')
+                    ->label('Jogos')
+                    ->limit(50),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPlaylists::route('/'),
+            'create' => Pages\CreatePlaylist::route('/create'),
+            'edit' => Pages\EditPlaylist::route('/{record}/edit'),
+        ];
+    }
+}
