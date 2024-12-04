02e9c971d6076cb7db8616424f8382062c9f4740
ThemerV <56089478+ThemerV@users.noreply.github.com>
2024-12-04 00:08:35 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,18 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        if (env('DB_CONNECTION') === 'pgsql') {
-            DB::beginTransaction();
-            DB::statement('SET CONSTRAINTS ALL DEFERRED;');
-            $tables = DB::select("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public'");
-            foreach ($tables as $table) {
-                $tableName = $table->tablename;
-                DB::statement("ALTER TABLE {$tableName} DISABLE TRIGGER ALL;");
-            }
-        } else if (env('DB_CONNECTION') === 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
         $this->call([
             ArtworkSeeder::class,
@@ -68,15 +57,7 @@ class DatabaseSeeder extends Seeder
             }
         });
 
-        if (env('DB_CONNECTION') === 'pgsql') {
-            DB::commit();
-            DB::statement('SET CONSTRAINTS ALL IMMEDIATE;');
-            foreach ($tables as $table) {
-                $tableName = $table->tablename;
-                DB::statement("ALTER TABLE {$tableName} ENABLE TRIGGER ALL;");
-            }
-        } else if (env('DB_CONNECTION') === 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
     }
 }
